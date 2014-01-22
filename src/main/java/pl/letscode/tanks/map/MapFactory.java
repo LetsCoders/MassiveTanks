c63eba844c4222f45a94dee2e7e81a6ade6db339
Tony <Tony@Tony-PC.home>
2014-01-22 15:31:58 +0100

    Fixed map borders

--- src/main/java/pl/letscode/tanks/map/MapFactory.java
+++ src/main/java/pl/letscode/tanks/map/MapFactory.java
@@ -1,5 +1,8 @@
 package pl.letscode.tanks.map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import pl.letscode.tanks.engine.objects.Position;
 import pl.letscode.tanks.map.terrain.RandomTerrainProvider;
 import pl.letscode.tanks.map.terrain.TerrainObject;
@@ -10,6 +13,8 @@ import com.google.inject.Inject;
 
 public class MapFactory {
 
+	private final static Logger LOG = LoggerFactory.getLogger(MapFactory.class);
+
 	private final TerrainObjectFactory objectFactory;
 	private final RandomTerrainProvider randomTerrainProvider;
 
@@ -19,7 +24,7 @@ public class MapFactory {
 		this.objectFactory = objectFactory;
 		this.randomTerrainProvider = randomTerrainProvider;
 	}
-	
+
 	public MapMatrix createEmptyMap(int height, int width) {
 		return new MapMatrix(height, width);
 	}
@@ -28,6 +33,19 @@ public class MapFactory {
 		MapMatrix result = new MapMatrix(height, width);
 		this.fillBorders(result);
 		this.fillWithRandomBlocks(result);
+		String log = "";
+		for (int i = 0; i < result.getMatrix().length; i++) {
+			for (int j = 0; j < result.getMatrix()[i].length; j++) {
+				TerrainObject terrainObject = result.getMatrix()[i][j];
+				if (terrainObject == null) {
+					log.concat("0,");
+				} else {
+					log.concat(terrainObject.getType().ordinal() + ",");
+				}
+			}
+			log.concat("/n");
+		}
+		LOG.info(log);
 		return result;
 	}
 
@@ -38,14 +56,16 @@ public class MapFactory {
 			matrix[i][0].setPosition(new Position(i * 32, 0));
 			matrix[i][mapMatrix.getHeight() + 1] = objectFactory
 					.buildTerrainBlock(TerrainType.SOLID);
-			matrix[i][mapMatrix.getHeight() + 1].setPosition(new Position(i * 32, (mapMatrix.getHeight() + 1) * 32));
+			matrix[i][mapMatrix.getHeight() + 1].setPosition(new Position(
+					i * 32, (mapMatrix.getHeight() + 1) * 32));
 		}
 		for (int i = 0; i < matrix[0].length; i++) {
 			matrix[0][i] = objectFactory.buildTerrainBlock(TerrainType.SOLID);
-			matrix[i][0].setPosition(new Position(i * 32, 0));
+			matrix[0][i].setPosition(new Position(0, i * 32));
 			matrix[mapMatrix.getWidth() + 1][i] = objectFactory
 					.buildTerrainBlock(TerrainType.SOLID);
-			matrix[i][mapMatrix.getHeight() + 1].setPosition(new Position(i * 32, (mapMatrix.getHeight() + 1) * 32));
+			matrix[mapMatrix.getWidth() + 1][i].setPosition(new Position(
+					(mapMatrix.getWidth() + 1) * 32, i * 32));
 		}
 	}
 
@@ -55,7 +75,8 @@ public class MapFactory {
 				mapMatrix.setTerrainObject(new Coord(i, j),
 						this.randomTerrainProvider.getRandom());
 				if (mapMatrix.exists(i, j))
-					mapMatrix.getMatrix()[i][j].setPosition(new Position(i * 32, j * 32));
+					mapMatrix.getMatrix()[i][j].setPosition(new Position(
+							i * 32, j * 32));
 			}
 		}
 	}
